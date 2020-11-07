aab382e2546a2a1918ec034551680af739b9eac7
Yohei Tamura <tamuhey@gmail.com>
2020-11-07 23:29:15 +0900

    up

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,6 @@ fn main() {
     let index = repo.index().unwrap();
     for ent in index.iter() {
         let p = String::from_utf8(ent.path).unwrap();
-        println!("{} {}", p, ent.ctime.seconds());
+        println!("{} {} {}", p, ent.ctime.seconds(), ent.mtime.seconds());
     }
 }
